192adee7a2b1ab4c54ab7d421ba49dffe8c08348
Kuvernoori <sanjarberkamalov@gmail.com>
2025-11-11 13:29:40 +0500

    UsersController

--- Controllers/UsersControllers.cs
+++ Controllers/UsersControllers.cs
@@ -1,8 +1,31 @@
-﻿namespace RytnavaBack.Controllers;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using RytnavaBack.Data;
 using RytnavaBack.Models;
-public class UsersControllers
+namespace RytnavaBack.Controllers;
+[ApiController]
+[Route("api/[controller]")]
+public class UsersControllers : ControllerBase
 {
-    
+    private readonly RytnavaContext _context;
+
+    public UsersControllers(RytnavaContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var users = await _context.Users.ToListAsync();
+        return Ok(users);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(User user)
+    {
+        _context.Users.Add(user);
+        await _context.SaveChangesAsync();
+        return CreatedAtAction(nameof(GetAll), new { id = user.Id }, user);
+    }
 }
